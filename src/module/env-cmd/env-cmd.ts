8282eedc0fb67128d8a3cff374c8420f74203354
spetushkou <sergey.petushkou@gmail.com>
2023-08-22 12:49:51 +0200

    feat: :rocket: #NOREF-0

---
+++ src/module/env-cmd/env-cmd.ts
@@ -0,0 +1,14 @@
+/**
+ * copy content from .env_demo to .env
+ * npm run demo:env-cmd
+ * npm run demo:env-cmd-async
+ * NODE_ENV=development npx env-cmd npx ts-node ./src/module/env-cmd/env-cmd.ts
+ */
+
+const { NODE_ENV, MONGODB_USER, MONGODB_PASSWORD } = process.env;
+
+console.log({
+  NODE_ENV,
+  MONGODB_USER,
+  MONGODB_PASSWORD,
+});
